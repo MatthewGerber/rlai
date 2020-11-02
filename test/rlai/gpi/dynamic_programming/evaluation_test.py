fb29196a3e2a792e011fed17964bceca745852a1
Matthew Gerber <matthew.gerber@dexcom.com>
2020-11-02 09:30:12 -0500

    Restructure tests.

--- test/rlai/gpi/dynamic_programming/evaluation_test.py
+++ test/rlai/gpi/dynamic_programming/evaluation_test.py
@@ -6,7 +6,6 @@ from numpy.random import RandomState
 
 from rlai.agents.mdp import StochasticMdpAgent
 from rlai.gpi.dynamic_programming.evaluation import evaluate_v_pi, evaluate_q_pi
-from rlai.gpi.dynamic_programming.iteration import iterate_value_v_pi, iterate_value_q_pi, iterate_policy_v_pi, iterate_policy_q_pi
 from rlai.environments.mdp import Gridworld
 
 
@@ -111,99 +110,3 @@ def test_evaluate_q_pi():
     assert q_pi == fixture
 
 
-def test_policy_iteration():
-
-    random_state = RandomState(12345)
-
-    mdp_environment: Gridworld = Gridworld.example_4_1(random_state)
-
-    # state-value policy iteration
-    mdp_agent_v_pi = StochasticMdpAgent(
-        'test',
-        random_state,
-        1
-    )
-
-    mdp_agent_v_pi.initialize_equiprobable_policy(mdp_environment.SS)
-
-    iterate_policy_v_pi(
-        mdp_agent_v_pi,
-        0.001,
-        True
-    )
-
-    # action-value policy iteration
-    mdp_agent_q_pi = StochasticMdpAgent(
-        'test',
-        random_state,
-        1
-    )
-
-    mdp_agent_q_pi.initialize_equiprobable_policy(mdp_environment.SS)
-
-    iterate_policy_q_pi(
-        mdp_agent_q_pi,
-        0.001,
-        True
-    )
-
-    # should get the same policy
-    assert mdp_agent_v_pi.pi == mdp_agent_q_pi.pi
-
-
-def test_value_iteration():
-
-    random_state = RandomState(12345)
-
-    mdp_environment: Gridworld = Gridworld.example_4_1(random_state)
-
-    # run policy iteration on v_pi
-    mdp_agent_v_pi_policy_iteration = StochasticMdpAgent(
-        'test',
-        random_state,
-        1
-    )
-
-    mdp_agent_v_pi_policy_iteration.initialize_equiprobable_policy(mdp_environment.SS)
-
-    iterate_policy_v_pi(
-        mdp_agent_v_pi_policy_iteration,
-        0.001,
-        True
-    )
-
-    # run value iteration on v_pi
-    mdp_agent_v_pi_value_iteration = StochasticMdpAgent(
-        'test',
-        random_state,
-        1
-    )
-
-    mdp_agent_v_pi_value_iteration.initialize_equiprobable_policy(mdp_environment.SS)
-
-    iterate_value_v_pi(
-        mdp_agent_v_pi_value_iteration,
-        0.001,
-        1,
-        True
-    )
-
-    assert mdp_agent_v_pi_policy_iteration.pi == mdp_agent_v_pi_value_iteration.pi
-
-    # run value iteration on q_pi
-    mdp_agent_q_pi_value_iteration = StochasticMdpAgent(
-        'test',
-        random_state,
-        1
-    )
-
-    mdp_agent_q_pi_value_iteration.initialize_equiprobable_policy(mdp_environment.SS)
-
-    iterate_value_q_pi(
-        mdp_agent_q_pi_value_iteration,
-        0.001,
-        1,
-        True
-    )
-
-    assert mdp_agent_q_pi_value_iteration.pi == mdp_agent_v_pi_policy_iteration.pi
--- test/rlai/gpi/monte_carlo/evaluation_test.py
+++ test/rlai/gpi/monte_carlo/evaluation_test.py
@@ -6,7 +6,6 @@ from numpy.random import RandomState
 from rlai.agents.mdp import StochasticMdpAgent
 from rlai.environments.mdp import Gridworld
 from rlai.gpi.monte_carlo.evaluation import evaluate_v_pi, evaluate_q_pi
-from rlai.gpi.monte_carlo.iteration import iterate_value_q_pi
 
 
 def test_evaluate_v_pi():
@@ -87,99 +86,3 @@ def test_evaluate_q_pi():
         fixture = pickle.load(file)
 
     assert q_pi == fixture
-
-
-def test_iterate_value_q_pi():
-
-    random_state = RandomState(12345)
-
-    mdp_environment: Gridworld = Gridworld.example_4_1(random_state)
-
-    mdp_agent = StochasticMdpAgent(
-        'test',
-        random_state,
-        1
-    )
-
-    mdp_agent.initialize_equiprobable_policy(mdp_environment.SS)
-
-    iterate_value_q_pi(
-        agent=mdp_agent,
-        environment=mdp_environment,
-        num_improvements=3000,
-        num_episodes_per_improvement=1,
-        update_upon_every_visit=False,
-        epsilon=0.1,
-        num_improvements_per_plot=None
-    )
-
-    # pickle doesn't like to unpickle instances with custom __hash__ functions
-    pi = {
-        s.i: {
-            a: mdp_agent.pi[s][a]
-            for a in mdp_agent.pi[s]
-        }
-        for s in mdp_agent.pi
-    }
-
-    # uncomment the following line and run test to update fixture
-    # with open(f'{os.path.dirname(__file__)}/fixtures/test_monte_carlo_iteration_of_value_q_pi.pickle', 'wb') as file:
-    #     pickle.dump(pi, file)
-
-    with open(f'{os.path.dirname(__file__)}/fixtures/test_monte_carlo_iteration_of_value_q_pi.pickle', 'rb') as file:
-        fixture = pickle.load(file)
-
-    assert pi == fixture
-
-
-def test_off_policy_monte_carlo():
-
-    random_state = RandomState(12345)
-
-    mdp_environment: Gridworld = Gridworld.example_4_1(random_state)
-
-    # target agent
-    mdp_agent = StochasticMdpAgent(
-        'test',
-        random_state,
-        1
-    )
-    mdp_agent.initialize_equiprobable_policy(mdp_environment.SS)
-
-    # episode generation (behavior) policy
-    off_policy_agent = StochasticMdpAgent(
-        'test',
-        random_state,
-        1
-    )
-    off_policy_agent.initialize_equiprobable_policy(mdp_environment.SS)
-
-    iterate_value_q_pi(
-        agent=mdp_agent,
-        environment=mdp_environment,
-        num_improvements=100,
-        num_episodes_per_improvement=1,
-        update_upon_every_visit=True,
-        epsilon=0.0,
-        off_policy_agent=off_policy_agent,
-        num_improvements_per_plot=None
-    )
-
-    # pickle doesn't like to unpickle instances with custom __hash__ functions
-    pi = {
-        s.i: {
-            a: mdp_agent.pi[s][a]
-            for a in mdp_agent.pi[s]
-        }
-        for s in mdp_agent.pi
-    }
-
-    # uncomment the following line and run test to update fixture
-    # with open(f'{os.path.dirname(__file__)}/fixtures/test_monte_carlo_off_policy_iteration_of_value_q_pi.pickle', 'wb') as file:
-    #     pickle.dump(pi, file)
-
-    with open(f'{os.path.dirname(__file__)}/fixtures/test_monte_carlo_off_policy_iteration_of_value_q_pi.pickle', 'rb') as file:
-        fixture = pickle.load(file)
-
-    assert pi == fixture
-
