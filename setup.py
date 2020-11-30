8cb1a99e7aa108a260b9425bb7a6d61b3952b7fd
Matthew Gerber <matthew.gerber@dexcom.com>
2020-11-29 23:07:27 -0500

    Bump version: 0.11.0 → 0.12.0.dev0

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ with open(path.join(path.abspath(path.dirname(__file__)), 'README.md'), encoding
 
 setup(
     name='rlai',
-    version='0.11.0',
+    version='0.12.0.dev0',
     description='Reinforcement Learning:  An Introduction',
     long_description=long_description,
     long_description_content_type='text/markdown',
