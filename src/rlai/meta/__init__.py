4fb685a558dc8818aeade1767817ad1e3fb4891c
Matthew Gerber <matthew.gerber@dexcom.com>
2020-12-06 22:27:59 -0500

    Try new docs layout.

--- src/rlai/meta/__init__.py
+++ src/rlai/meta/__init__.py
@@ -71,7 +71,8 @@ def summarize(
                         if page not in chapter_page_descriptions[chapter]:
                             chapter_page_descriptions[chapter][page] = []
 
-                        chapter_page_descriptions[chapter][page].append(f'## `{full_path}`\n```\n{attribute.__doc__.strip()}\n```\n')
+                        src_path = f'../src/{full_path.replace(".", "/").rsplit("/", maxsplit=1)[0]}.py'
+                        chapter_page_descriptions[chapter][page].append(f'### [{full_path}]({src_path})\n```\n{attribute.__doc__.strip()}\n```\n')
 
                         paths_summarized.add(full_path)
 
@@ -86,7 +87,28 @@ def main():
     # noinspection PyTypeChecker
     summarize(rlai, chapter_page_descriptions)
 
-    meta_md_path = f'{os.path.dirname(__file__)}/../../../docs/index.md'
+    docs_dir = f'{os.path.dirname(__file__)}/../../../docs/'
+    meta_md_path = f'{docs_dir}index.md'
+
+    ch_num_name = {
+        1: 'Introduction',
+        2: 'Multi-armed Bandits',
+        3: 'Finite Markov Decision Processes',
+        4: 'Dynamic Programming',
+        5: 'Monte Carlo Methods',
+        6: 'Temporal-Difference Learning',
+        7: 'n-step Bootstrapping',
+        8: 'Planning and Learning with Tabular Methods',
+        9: 'On-policy Prediction with Approximation',
+        10: 'On-policy Control with Approximation',
+        11: 'Off-policy Methods with Approximation',
+        12: 'Eligibility Traces',
+        13: 'Policy Gradient Methods',
+        14: 'Psychology',
+        15: 'Neuroscience',
+        16: 'Applications and Case Studies',
+        17: 'Frontiers'
+    }
 
     # read base readme
     with open('./README_base.md', 'r') as base:
@@ -95,31 +117,29 @@ def main():
     # write markdown file
     with open(meta_md_path, 'w') as meta_md:
 
-        meta_md.write(f'<!--TOC-->\n\n{readme_base}\n\n')
+        meta_md.write(f'{readme_base}\n\n')
 
         # write sorted string chapters
+        meta_md.write('# General Topics\n')
         for chapter in sorted(filter(lambda ch: isinstance(ch, str), chapter_page_descriptions)):
-            meta_md.write(f'# {chapter}\n')
-            for page in sorted(chapter_page_descriptions[chapter]):
-                for description in sorted(chapter_page_descriptions[chapter][page]):
-                    meta_md.write(description)
+            ch_filename = f'ch_{chapter.replace(" ", "_")}.md'
+            meta_md.write(f'### [{chapter}]({ch_filename})\n')
+            with open(f'{docs_dir}{ch_filename}', 'w') as ch_md:
+                ch_md.write(f'# {chapter}\n')
+                for page in sorted(chapter_page_descriptions[chapter]):
+                    for description in sorted(chapter_page_descriptions[chapter][page]):
+                        ch_md.write(description)
 
         # write sorted numeric chapters
+        meta_md.write('\n# Book Chapters\n')
         for chapter in sorted(filter(lambda ch: isinstance(ch, int), chapter_page_descriptions)):
-            meta_md.write(f'# Chapter {chapter}\n')
-            for page in sorted(chapter_page_descriptions[chapter]):
-                for description in sorted(chapter_page_descriptions[chapter][page]):
-                    meta_md.write(description)
-
-    # generate toc
-    toc = md_toc.build_toc(meta_md_path)
-
-    # write full markdown file
-    with open(meta_md_path, 'r') as meta_md:
-        markdown_with_toc = f'# Table of Contents\n{toc}{meta_md.read()}'
-
-    with open(meta_md_path, 'w') as meta_md:
-        meta_md.write(markdown_with_toc)
+            ch_filename = f'ch_{chapter}.md'
+            meta_md.write(f'### [Chapter {chapter}:  {ch_num_name[chapter]}]({ch_filename})\n')
+            with open(f'{docs_dir}{ch_filename}', 'w') as ch_md:
+                ch_md.write(f'# Chapter {chapter}:  {ch_num_name[chapter]}\n')
+                for page in sorted(chapter_page_descriptions[chapter]):
+                    for description in sorted(chapter_page_descriptions[chapter][page]):
+                        ch_md.write(description)
 
 
 if __name__ == '__main__':
