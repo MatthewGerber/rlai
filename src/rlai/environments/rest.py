7070aa2535a99a4eff066f941cccf0d85c8f319d
Matthew Gerber <matthew.gerber@dexcom.com>
2021-02-22 23:40:26 -0500

    Cleanup

--- src/rlai/environments/rest.py
+++ src/rlai/environments/rest.py
@@ -4,7 +4,7 @@ from abc import ABC, abstractmethod
 from functools import partial
 from typing import Tuple, Optional, Any, Dict
 
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, Response
 from numpy.random import RandomState
 
 from rlai.actions import Action
@@ -30,65 +30,83 @@ class RestMdpEnvironment(MdpEnvironment, ABC):
         Reset the environment for a new run.
 
         :param self: Reference to the environment. The current function is called by Flask with no arguments, which is
-        why we set up a partial function, pass it the environment, and mark the current function static.
+        why we set up a partial function, pass it the environment, and mark the current function static. If the
+        function is not marked static, then the self instance and the partial argument both get passed.
         :return: Empty string.
         """
 
-        self.state = self.init_state_from_rest_request_dict(request.json, False)
-        self.reset_event.set()
+        self.state, _ = self.init_state_from_rest_put_dict(request.json)
+        self.client_reset_for_new_run.set()
+
+        return ""
+
+    @staticmethod
+    def rest_get_action(
+            self
+    ) -> Response:
+        """
+        Get the local environment's next action, to be used in advancing the calling environment.
+
+        :param self: Reference to the environment. The current function is called by Flask with no arguments, which is
+        why we set up a partial function, pass it the environment, and mark the current function static. If the
+        function is not marked static, then the self instance and the partial argument both get passed.
+        :return: Response containing the action to be used to advance the calling environment.
+        """
+
+        # wait for the server to set the action
+        self.server_set_action.wait()
+        self.server_set_action.clear()
+
+        return jsonify(
+            action=self.action.__dict__
+        )
+
+    @staticmethod
+    def rest_set_state(
+            self
+    ) -> str:
+        """
+        Set the local environment's next state, which resulted from the action returned to the calling environment.
+
+        :param self: Reference to the environment. The current function is called by Flask with no arguments, which is
+        why we set up a partial function, pass it the environment, and mark the current function static. If the
+        function is not marked static, then the self instance and the partial argument both get passed.
+        :return: Empty string.
+        """
+
+        self.state, self.reward = self.init_state_from_rest_put_dict(request.json)
+        self.client_set_state_and_reward.set()
 
         return ""
 
     @abstractmethod
-    def init_state_from_rest_request_dict(
+    def init_state_from_rest_put_dict(
             self,
-            rest_request_dict: Dict[Any, Any],
-            terminal: bool
-    ) -> MdpState:
+            rest_request_dict: Dict[Any, Any]
+    ) -> Tuple[MdpState, Reward]:
         """
-        Initialize a state from its REST request dictionary.
+        Initialize a state from the dictionary provided by the REST PUT (e.g., for setting and resetting the state).
 
-        :param rest_request_dict: REST request dictionary.
-        :param terminal: Whether or not the state is terminal.
-        :return: State.
+        :param rest_request_dict: REST PUT dictionary.
+        :return: 2-tuple of the state and reward.
         """
 
     def reset_for_new_run(
             self,
             agent: MdpAgent
     ) -> MdpState:
+        """
+        Reset the environment for a new run.
 
-        self.reset_event.wait()
-        self.reset_event.clear()
+        :param agent: Agent.
+        :return: Initial state.
+        """
+
+        # wait for the client to reset
+        self.client_reset_for_new_run.wait()
+        self.client_reset_for_new_run.clear()
 
         return self.state
-
-    @staticmethod
-    def rest_get_action(
-            self
-    ):
-        self.action_event.wait()
-        self.action_event.clear()
-
-        return jsonify(
-            action=self.action.name,
-            value=1.0
-        )
-
-    @staticmethod
-    def rest_set_state(
-            self
-    ):
-        dead = request.json['dead']
-        win = request.json['win']
-        terminal = dead or win
-        reward = 1.0 if win else 0.0
-
-        self.state = self.init_state_from_rest_request_dict(request.json, terminal=terminal)
-        self.reward = Reward(None, reward)
-        self.state_reward_event.set()
-
-        return ""
 
     def advance(
             self,
@@ -97,12 +115,24 @@ class RestMdpEnvironment(MdpEnvironment, ABC):
             a: Action,
             agent: Agent
     ) -> Tuple[MdpState, Reward]:
+        """
+        Advance the simulation environment by setting the action for the client to retrieve and waiting for the client
+        to send the updated state back.
 
+        :param state: Current state.
+        :param t: Current time step.
+        :param a: Action for the client environment to take.
+        :param agent: Agent.
+        :return: Next state and reward.
+        """
+
+        # set action for client to retrieve and signal event
         self.action = a
-        self.action_event.set()
+        self.server_set_action.set()
 
-        self.state_reward_event.wait()
-        self.state_reward_event.clear()
+        # wait for the client to reply
+        self.client_set_state_and_reward.wait()
+        self.client_set_state_and_reward.clear()
 
         return self.state, self.reward
 
@@ -128,8 +158,10 @@ class RestMdpEnvironment(MdpEnvironment, ABC):
 
         self.flask_app = Flask(__name__)
 
-        self.reset_event = threading.Event()
-        self.reset_event.clear()
+        # set up REST endpoints
+
+        self.client_reset_for_new_run = threading.Event()
+        self.client_reset_for_new_run.clear()
         self.flask_app.add_url_rule(
             rule='/reset-for-new-run',
             endpoint='reset-for-new-run',
@@ -138,8 +170,8 @@ class RestMdpEnvironment(MdpEnvironment, ABC):
         )
 
         self.action: Optional[Action] = None
-        self.action_event = threading.Event()
-        self.action_event.clear()
+        self.server_set_action = threading.Event()
+        self.server_set_action.clear()
         self.flask_app.add_url_rule(
             rule='/get-action',
             endpoint='get-action',
@@ -149,8 +181,8 @@ class RestMdpEnvironment(MdpEnvironment, ABC):
 
         self.state: Optional[MdpState] = None
         self.reward: Optional[Reward] = None
-        self.state_reward_event = threading.Event()
-        self.state_reward_event.clear()
+        self.client_set_state_and_reward = threading.Event()
+        self.client_set_state_and_reward.clear()
         self.flask_app.add_url_rule(
             rule='/set-state',
             endpoint='set-state',
@@ -158,6 +190,7 @@ class RestMdpEnvironment(MdpEnvironment, ABC):
             methods=['PUT']
         )
 
+        # run flask server in separate thread
         self.flask_thread = threading.Thread(
             target=self.flask_app.run,
             kwargs={
@@ -166,6 +199,7 @@ class RestMdpEnvironment(MdpEnvironment, ABC):
             }
         )
 
+        # disable noisy logging
         # log = logging.getLogger('werkzeug')
         # log.disabled = True
 
--- src/rlai/environments/robocode.py
+++ src/rlai/environments/robocode.py
@@ -1,12 +1,14 @@
 from typing import List, Union, Tuple, Dict, Any, Optional
 
 import numpy as np
+from flask import request
 from numpy.random import RandomState
 
 from rlai.actions import Action
 from rlai.environments.mdp import MdpEnvironment
 from rlai.environments.rest import RestMdpEnvironment
 from rlai.meta import rl_text
+from rlai.rewards import Reward
 from rlai.states.mdp import MdpState
 from rlai.value_estimation.function_approximation.models import StateActionInteractionFeatureExtractor
 
@@ -14,7 +16,8 @@ from rlai.value_estimation.function_approximation.models import StateActionInter
 @rl_text(chapter='Environments', page=1)
 class RobocodeEnvironment(RestMdpEnvironment):
     """
-    Robocode environment.
+    Robocode environment, provided by the Java implementation via calls to REST endpoints served by the inherited
+    `RestMdpEnvironment`.
     """
 
     @classmethod
@@ -23,24 +26,47 @@ class RobocodeEnvironment(RestMdpEnvironment):
             args: List[str],
             random_state: RandomState
     ) -> Tuple[MdpEnvironment, List[str]]:
+        """
+        Initialize an environment from arguments.
+
+        :param args: Arguments.
+        :param random_state: Random state.
+        :return: 2-tuple of an environment and a list of unparsed arguments.
+        """
 
         return RobocodeEnvironment(
-            name='test',
+            name='robocode',
             random_state=random_state,
             T=None
         ), args
 
-    def init_state_from_rest_request_dict(
+    def init_state_from_rest_put_dict(
             self,
-            rest_request_dict: Dict[Any, Any],
-            terminal: bool
-    ) -> MdpState:
+            rest_request_dict: Dict[Any, Any]
+    ) -> Tuple[MdpState, Reward]:
+        """
+        Initialize a state from the dictionary provided by the REST PUT (e.g., for setting and resetting the state).
 
-        return RobocodeState(
+        :param rest_request_dict: REST PUT dictionary.
+        :return: 2-tuple of the state and reward.
+        """
+
+        dead = request.json['dead']
+        win = request.json['win']
+        terminal = dead or win
+
+        state = RobocodeState(
             **rest_request_dict,
             actions=self.robot_actions,
             terminal=terminal
         )
+
+        reward = Reward(
+            i=None,
+            r=1.0 if win else 0.0
+        )
+
+        return state, reward
 
     def __init__(
             self,
@@ -63,13 +89,17 @@ class RobocodeEnvironment(RestMdpEnvironment):
         )
 
         self.robot_actions = [
-            Action(0, 'ahead'),
-            Action(1, 'back'),
-            Action(2, 'fire')
+            RobocodeAction(0, 'ahead', 50.0),
+            RobocodeAction(1, 'back', 50.0),
+            RobocodeAction(2, 'fire', 1.0)
         ]
 
 
+@rl_text(chapter='States', page=1)
 class RobocodeState(MdpState):
+    """
+    State of Robocode battle.
+    """
 
     def __init__(
             self,
@@ -81,6 +111,18 @@ class RobocodeState(MdpState):
             actions: List[Action],
             terminal: bool
     ):
+        """
+        Initialize the state.
+
+        :param x: Robot x position.
+        :param y: Robot y position.
+        :param scanned_robot_event: Event information for scanning of another robot.
+        :param dead: Whether or not the robot is dead.
+        :param win: Whether or not the robot has won.
+        :param actions: List of actions that can be taken.
+        :param terminal: Whether or not the state is terminal.
+        """
+
         super().__init__(
             i=None,
             AA=actions,
@@ -94,7 +136,11 @@ class RobocodeState(MdpState):
         self.win = win
 
 
+@rl_text(chapter='Feature Extractors', page=1)
 class RobocodeFeatureExtractor(StateActionInteractionFeatureExtractor):
+    """
+    Robocode feature extractor.
+    """
 
     @classmethod
     def init_from_arguments(
@@ -116,3 +162,31 @@ class RobocodeFeatureExtractor(StateActionInteractionFeatureExtractor):
             [1, 2, 3]
             for a in actions
         ])
+
+
+@rl_text(chapter='Actions', page=1)
+class RobocodeAction(Action):
+    """
+    Robocode action.
+    """
+
+    def __init__(
+            self,
+            i: int,
+            name: str,
+            value: float
+    ):
+        """
+        Initialize the action.
+
+        :param i: Identifier for the action.
+        :param name: Name.
+        :param value: Value.
+        """
+
+        super().__init__(
+            i=i,
+            name=name
+        )
+
+        self.value = value
