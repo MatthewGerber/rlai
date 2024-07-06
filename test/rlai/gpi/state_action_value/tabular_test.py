e543c1bb6da124ae211645388f4e8a8fd9196443
MatthewGerber <mvg0419@dexcom.com>
2024-07-06 08:48:29 -0400

    Finish refactoring to match src layout.

--- test/rlai/gpi/state_action_value/tabular_test.py
+++ test/rlai/gpi/state_action_value/tabular_test.py
@@ -1,6 +1,9 @@
 import numpy as np
 import pytest
+from numpy.random import RandomState
 
+from rlai.core.environments.gridworld import Gridworld
+from rlai.gpi.state_action_value import ActionValueMdpAgent
 from rlai.gpi.state_action_value.tabular import TabularPolicy
 from rlai.gpi.state_action_value.tabular import TabularStateActionValueEstimator
 
@@ -39,3 +42,27 @@ def test_invalid_epsilon():
 
     with pytest.raises(ValueError, match='epsilon must be >= 0'):
         TabularStateActionValueEstimator(None, -1, None)
+
+
+def test_invalid_improve_policy_with_q_pi():
+    """
+    Test.
+    """
+
+    random_state = RandomState(12345)
+    mdp_environment: Gridworld = Gridworld.example_4_1(random_state, None)
+    epsilon = 0.0
+    mdp_agent = ActionValueMdpAgent(
+        'test',
+        random_state,
+        1,
+        TabularStateActionValueEstimator(mdp_environment, epsilon, None)
+    )
+
+    assert isinstance(mdp_agent.pi, TabularPolicy)
+
+    with pytest.raises(ValueError, match='Epsilon must be >= 0'):
+        mdp_agent.pi.improve_with_q_pi(
+            {},
+            -1.0
+        )
